52a76a884da13f4607b3642ccff3fab04e6f2150
Akobir <akobirortiqov40@gmail.com>
2025-05-26 21:27:00 +0500

    database_settings

--- core/database_settings.py
+++ core/database_settings.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 import psycopg2
 from core.config import DB_CONFIG
 
@@ -14,3 +14,38 @@ class DatabaseManager:
          self.conn = psycopg2.connect(**DB_CONFIG)
          self.cursor = self.conn.cursor(cursor_factory=DictCursor)
          return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+              self.conn.rollback()
+        else:
+             self.conn.commit()
+    
+        if self.conn:
+             self.conn.close()
+
+        if self.cursor:
+             self.cursor.close()
+
+
+    def execute(self, query: str, params: Union[tuple, dict, None] = None):
+         self.cursor.execute(query, params)
+
+
+    def fetchone(self, query: str, params: Union[tuple, dict, None] = None):
+         self.cursor.execute(query, params)
+         return self.cursor.fetchone()
+
+
+    def fetchall(self, query: str, params: Union[tuple, dict, None] = None):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchall()
+
+
+def execute_query(
+           query: str, 
+           params: Union[tuple, dict, None] = None
+           fatch: Union[str, None] = None
+) -> None | DictRow | Union[tuple, dict, None]
+
